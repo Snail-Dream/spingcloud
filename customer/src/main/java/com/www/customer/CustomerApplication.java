4221040582e5b2185b02c9e9dfae0433a28e0b62
root <858900953@qq.com>
2019-12-26 20:53:33 +0800

    ribbon

--- customer/src/main/java/com/www/customer/CustomerApplication.java
+++ customer/src/main/java/com/www/customer/CustomerApplication.java
@@ -2,15 +2,19 @@ package com.www.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
+//@EnableEurekaClient
 public class CustomerApplication {
 
     @Bean
+    @LoadBalanced //开启负载均衡
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
--- customer/src/main/java/com/www/customer/controller/CustomerController.java
+++ customer/src/main/java/com/www/customer/controller/CustomerController.java
@@ -1,9 +1,13 @@
 package com.www.customer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * @author ：www
@@ -16,6 +20,8 @@ import org.springframework.web.client.RestTemplate;
 public class CustomerController {
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private DiscoveryClient discoveryClient;
     @GetMapping(value = "customer")
     public String customer(){
         return "this is customer";
@@ -23,5 +29,22 @@ public class CustomerController {
     @GetMapping(value = "select")
     public String select(){
         return restTemplate.getForObject("http://localhost:18092/service",String.class);
+    }
+    @GetMapping(value = "selectDiscoveryClient")
+    public String selectDiscoveryClient(){
+        List<ServiceInstance> server = discoveryClient.getInstances("service");
+        ServiceInstance serviceInstance = server.get(0);
+        return restTemplate.getForObject("http://"+serviceInstance.getHost()+":"+serviceInstance.getPort()+"/service",String.class);
+    }
+    /** 
+    * @Description:  通过负载均衡调用
+    * @Param: [] 
+    * @return: java.lang.String 
+    * @Author: www
+    * @Date: 19-12-26 
+    */ 
+    @GetMapping(value = "selectFZJH")
+    public String selectFZJH(){
+        return restTemplate.getForObject("http://service/service",String.class);
     }
 }
